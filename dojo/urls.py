b2ed385c53fd4092249f5ba5c280a78c7fc382a8
Ben Woo <bufpal@gmail.com>
2018-05-08 10:11:58 +0900

    Function Base View : method 1, 2

--- dojo/urls.py
+++ dojo/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^sum/(?P<numbers>[\d/]+)/$', views.mysum),
     url(r'^hello/(?P<name>[ㄱ-힣]+)/(?P<age>\d+)/$', views.hello),
+    url(r'^list1/$', views.post_list1),
+    url(r'^list2/$', views.post_list2),
 ]
--- dojo/views.py
+++ dojo/views.py
@@ -9,3 +9,14 @@ def mysum(request, numbers):
 
 def hello(request, name, age):
     return HttpResponse("안녕하세요, {}, {}살 이시네요".format(name, age))
+
+def post_list1(request):
+    name = "Ben"
+    return HttpResponse("""<h1>Hello {name} </h1>
+<p> welcome to the your World""".format(name=name)
+    )
+
+def post_list2(request):
+    name = {"name": "Ben"}
+    response = render(request, "dojo/post_list2.html", name)
+    return response
