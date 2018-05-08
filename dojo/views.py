0125a07f6ee161adf01132a5a8618d524d99e688
Ben Woo <bufpal@gmail.com>
2018-05-08 11:08:22 +0900

    Function Based View method3:JsonResponse, method4:file(excel) download

--- dojo/views.py
+++ dojo/views.py
@@ -1,5 +1,8 @@
-from django.http import HttpResponse
+import os
+from django.conf import settings
+from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+
 
 def mysum(request, numbers):
     # numbers = [int(num) for num in numbers.split("/")]
@@ -7,8 +10,10 @@ def mysum(request, numbers):
     result = sum(map(lambda s: int(s or 0), numbers.split("/")))
     return HttpResponse(result)
 
+
 def hello(request, name, age):
     return HttpResponse("안녕하세요, {}, {}살 이시네요".format(name, age))
+
 
 def post_list1(request):
     name = "Ben"
@@ -16,7 +21,24 @@ def post_list1(request):
 <p> welcome to the your World""".format(name=name)
     )
 
+
 def post_list2(request):
     name = {"name": "Ben"}
     response = render(request, "dojo/post_list2.html", name)
     return response
+
+
+def post_list3(request):
+    json_response = JsonResponse({
+        "items": ["python", "django", "data", "algorithm"],
+        "message": "road to python and django developer",
+    }, json_dumps_params={"ensure_ascii": False})
+    return json_response
+
+def excel_download(request):
+    filepath = os.path.join(settings.BASE_DIR, "hotfoot-software.xls")
+    filename = os.path.basename(filepath)
+    with open(filepath, "rb") as f:
+        response = HttpResponse(f, content_type='application/vnd.ms-excel') # content_type 의 default 는 text
+        response['Content-disposition'] = 'attachment; filename="{}"'.format(filename)
+        return response
