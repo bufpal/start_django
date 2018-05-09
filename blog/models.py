30681bc4cd6b205ab27685be08d19255440045ed
Ben Woo <bufpal@gmail.com>
2018-05-09 11:51:33 +0900

    blog.models.Post create/migrate model, registered to admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Subject", help_text="input title, max 100")
+    content = models.TextField(verbose_name="Content")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
