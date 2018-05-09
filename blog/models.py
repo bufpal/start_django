82ad4f2f026d212f97a4cf14306a283c6d7ef4a4
Ben Woo <bufpal@gmail.com>
2018-05-09 12:32:39 +0900

    added tag, lnglat model field on blog.models.Post

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,21 @@
 from django.db import models
+from django.forms import ValidationError
+import re
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.\d+),([+-]?\d+\.\d+)$', value):
+        raise ValidationError('Invalid LngLat Type')
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name="Subject", help_text="input title, max 100")
     content = models.TextField(verbose_name="Content")
+    tag = models.CharField(max_length=50, blank=True, help_text="write tags")
+    lnglat = models.CharField(max_length=30,
+        validators=[lnglat_validator],
+        help_text="\"longtitude,latitude\" type",
+        blank=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
