855f2d10f0f41f2efc8189a9a615787efdb4495a
Ben Woo <bufpal@gmail.com>
2018-05-10 17:16:04 +0900

    custom content_type on blog.admin.PostAdmin

--- blog/admin.py
+++ blog/admin.py
@@ -8,5 +8,9 @@ from .models import Post
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    list_display = ['id', 'title', 'created_at', 'updated_at']
+    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
 
+
+    def content_size(self, post):
+        return "{}".format(len(post.content))
+    content_size.short_description="Content Length"
