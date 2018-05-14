587d832b2dd2d56db9a05d569ffb48b6d8e6a101
Ben Woo <bufpal@gmail.com>
2018-05-14 13:28:56 +0900

    renew accounts.models.Profile user field attrubute to settings.AUTH_USER_MODEL)

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.db import models
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.forms import ValidationError
 
 # def phone_validator(value):
@@ -7,8 +8,12 @@ from django.forms import ValidationError
 #         raise ValidationError('Wrong \"Phone Number\" format')
 
 class Profile(models.Model):
-    user = models.OneToOneField(User) #FIX: BadCase
+    # user = models.OneToOneField(User) #FIX: BadCase
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     phone_number = models.CharField(max_length=20,
         # validators=[phone_validator]    
     )
     address = models.CharField(max_length=50)
+
+    # def __str__(self):
+    #     self.user
