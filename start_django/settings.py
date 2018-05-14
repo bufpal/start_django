6391ba06f8cadf3d31881b23d3d8d3444a1b82d5
Ben Woo <bufpal@gmail.com>
2018-05-14 18:09:26 +0900

    set main template(start_django.templates.layout.html), set inheritance

--- start_django/settings.py
+++ start_django/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'start_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'start_django', 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
