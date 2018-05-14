5579985d40b45c551167df4f3ee406425fd73937
Ben Woo <bufpal@gmail.com>
2018-05-14 11:25:19 +0900

    added Profile model on accounts.models / customized in admin

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.forms import ValidationError
 
-# Create your models here.
+# def phone_validator(value):
+#     if not re.match(r'^$', value):
+#         raise ValidationError('Wrong \"Phone Number\" format')
+
+class Profile(models.Model):
+    user = models.OneToOneField(User) #FIX: BadCase
+    phone_number = models.CharField(max_length=20,
+        # validators=[phone_validator]    
+    )
+    address = models.CharField(max_length=50)
