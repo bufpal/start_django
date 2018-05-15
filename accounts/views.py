530c8f9ec4c09206a4ac31a11c32557870cfcb2b
Ben Woo <bufpal@gmail.com>
2018-05-15 15:48:31 +0900

    test render_to_string

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
+from django.template.loader import render_to_string
 from django.shortcuts import render
 
-# Create your views here.
+
+def welcome_message(request):
+    rendered = render_to_string('accounts/welcome_message.txt', {
+        'name': 'Brandon',
+        'date': '2018/5/14',
+    })
+    return HttpResponse(rendered)
